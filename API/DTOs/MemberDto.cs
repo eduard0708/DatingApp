e2acc952f9fe944f78e8904fa64cba4f7a1a86a0
Eduard <eduard.uytoco@gmail.com>
2021-08-22 13:55:23 +0300

    section 8 partend of section 8

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -1,18 +1,16 @@
-﻿using API.Extensions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace API.Entities
+namespace API.DTOs
 {
-    public class AppUser
+    public class MemberDto
     {
         public int Id { get; set; }
-        public string  UserName { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public string UserName { get; set; }
+        public string PhotoUrl { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
@@ -22,12 +20,6 @@ namespace API.Entities
         public string Interest { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public ICollection<Photo> Photos { get; set; }
-
-        public int GetAget()
-        {
-            return DateOfBirth.CalculateAge();
-        }
-
+        public ICollection<PhotoDto> Photos { get; set; }
     }
 }
